0075dd4bb2b8eae8263bdcc0623ee444a6529986
tekeuange23 <tekeuangeleonel@gmail.com>
2023-03-03 22:13:48 +0100

    project setup

---
+++ Guerrier.h
@@ -0,0 +1,14 @@
+#ifndef GUERRIER_H_INCLUDED
+#define GUERRIER_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include "Personnage.h"
+
+class Guerrier : public Personnage
+{
+
+};
+
+
+#endif // GUERRIER_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "personnage.h"
+#include "fonctions.h"
+
+using namespace std;
+
+
+int main()
+{
+
+
+    return 0;
+}
