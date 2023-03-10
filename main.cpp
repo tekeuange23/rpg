fbf644e5e933c037721e3273da2b169309463781
tekeuange23 <tekeuangeleonel@gmail.com>
2023-03-10 15:24:23 +0100

    adding save-party feature: implement IO saving and reading when quit

--- main.cpp
+++ main.cpp
@@ -6,7 +6,17 @@ using namespace std;
 
 int main()
 {
+    string const f("E:/ANGE/Projets/C++/jeu RPG/Save.txt");
+    ifstream fluxO(f.c_str());
+    string const g("E:/ANGE/Projets/C++/jeu RPG/Load.txt");
+    ofstream loadI(g.c_str());
 
+    cloon(loadI,fluxO);
+
+    ofstream fluxI(f.c_str());
+    ifstream loadO(g.c_str());
+
+    menu(fluxI,loadO);
 
     return 0;
 }
